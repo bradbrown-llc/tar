080802e0496f6d03f80f896099cfd9240de5d5e8
Brad Brown <bradbrown@bradbrown.llc>
2024-07-06 12:09:46 +0000

    options for create and extract with base64 encoding/decoding

--- Tar.ts
+++ Tar.ts
@@ -1,3 +1,5 @@
+import { encodeBase64, decodeBase64 } from 'https://deno.land/std@0.224.0/encoding/base64.ts';
+
 export class Tar {
 
     static create(dir:string) {
@@ -7,6 +9,8 @@ export class Tar {
         return cmdOut.stdout
     }
 
+    static create64(dir:string) { return encodeBase64(Tar.create(dir)) }
+
     static async extract(archive:Uint8Array, dir:string) {
         const args = ['xCfz', dir, '-']
         const proc = new Deno.Command('tar', { args, stdin: 'piped', stdout: 'piped' }).spawn()
@@ -14,6 +18,10 @@ export class Tar {
         await writer.write(archive)
         await writer.close()
         await proc.output()
+    }
+
+    static async extract64(archive:string, dir:string) {
+        return await Tar.extract(decodeBase64(archive), dir)
     }
 
 }
